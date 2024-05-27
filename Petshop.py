d6d86ee22d99cac7d0863e9f9097f7a9d8d9d78a
Isabelle Putri Ardini <iputriardin@gmail.com>
2024-05-27 12:33:21 +0700

    putri percabangan

--- Petshop.py
+++ Petshop.py
@@ -66,8 +66,22 @@ def print_nota(reservasi):
   print("Total Harga: Rp", total_harga)
   print("---------------------------------")
 
-    # if (pilihan ==1 ):
-    #     grooming_anjing
-    #     anjing = int(input("pilih grooming anjing"))
-    #     if (anjing == 1):
-    #         25000 * 1
+# Pilihan jenis hewan
+hewan = int(input("Pilih jenis hewan [1/2]: "))
+
+if hewan == 1:   
+# List untuk menyimpan treatment anjing
+    treatment_anjing = []
+
+# Percabangan pilihan anjing
+    while True:
+        hewan_anjing = int(input("Pilih treatment [1/6] atau pilih 0 jika sudah memilih: "))
+        if hewan_anjing == 0:
+            break
+
+        if 1 <= hewan_anjing <= 6:
+            harga = [40000, 50000, 60000, 70000, 80000, 100000]
+            treatment = ["Grooming kering", "Grooming basic", "Grooming shampoo Anti kutu", "Grooming shampoo Anti jamur", "Grooming Shampoo Anti kutu & jamur", "Grooming shampoo Whitening"]
+            treatment_anjing.append((treatment, harga))
+        else:
+            print("Pilihan tidak valid")
